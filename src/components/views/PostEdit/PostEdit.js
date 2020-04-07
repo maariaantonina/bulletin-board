31ae0a9b682e175cf82b8e5b16eb939b6a6d8d4b
Maria Kasińska <mariakasinska17@gmail.com>
2020-04-07 20:24:07 +0200

    able to get edit endpoint only if logged

--- src/components/views/PostEdit/PostEdit.js
+++ src/components/views/PostEdit/PostEdit.js
@@ -6,8 +6,11 @@ import clsx from 'clsx';
 
 import { connect } from 'react-redux';
 import { getById } from '../../../redux/postsRedux.js';
+import { isLogged } from '../../../redux/userRedux.js';
 
 //import styles from './PostEdit.module.scss';
+
+import { NotFound } from '../NotFound/NotFound';
 
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
@@ -38,42 +41,57 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const Component = ({ className, title, description, location, _id }) => {
+const Component = ({
+  className,
+  title,
+  description,
+  location,
+  _id,
+  logged,
+}) => {
   const classes = useStyles();
   return (
     <div className={clsx(className, classes.root)}>
-      <Paper elevation={3}>
-        <Typography variant="h5" color="textSecondary" component="h2">
-          Edit your post
-        </Typography>
-        <form noValidate autoComplete="off">
-          <div className={classes.form}>
-            <TextField
-              required
-              id="post-title"
-              label="Post title"
-              defaultValue={title}
-            />
-            <TextField
-              id="post-description"
-              label="Description"
-              multiline
-              rows="4"
-              defaultValue={description}
-            />
-            <TextField id="location" label="Location" defaultValue={location} />
-            <Button
-              size="medium"
-              color="primary"
-              variant="contained"
-              component={Link}
-              to={`/post/${_id}`}
-            >
-              Save
-            </Button>
-          </div>
-        </form>
-      </Paper>
+      {logged ? (
+        <Paper elevation={3}>
+          <Typography variant="h5" color="textSecondary" component="h2">
+            Edit your post
+          </Typography>
+          <form noValidate autoComplete="off">
+            <div className={classes.form}>
+              <TextField
+                required
+                id="post-title"
+                label="Post title"
+                defaultValue={title}
+              />
+              <TextField
+                id="post-description"
+                label="Description"
+                multiline
+                rows="4"
+                defaultValue={description}
+              />
+              <TextField
+                id="location"
+                label="Location"
+                defaultValue={location}
+              />
+              <Button
+                size="medium"
+                color="primary"
+                variant="contained"
+                component={Link}
+                to={`/post/${_id}`}
+              >
+                Save
+              </Button>
+            </div>
+          </form>
+        </Paper>
+      ) : (
+        <NotFound></NotFound>
+      )}
     </div>
   );
 };
@@ -86,12 +104,14 @@ Component.propTypes = {
   description: PropTypes.string,
   author: PropTypes.string,
   location: PropTypes.string,
+  logged: PropTypes.bool,
 };
 
 const mapStateToProps = (state, props) => {
   const post = getById(state, props.match.params.id);
   return {
     ...post,
+    logged: isLogged(state),
   };
 };
 
